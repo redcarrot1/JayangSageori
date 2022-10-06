4e1b47484070768bfc3ffa0afdc814044ab7d53b
IThinkSo! <51076814+redcarrot1@users.noreply.github.com>
2022-10-06 18:07:17 +0900

    feat: add User data class

---
+++ src/User.cpp
@@ -0,0 +1,39 @@
+#include "User.h"
+#include <iostream>
+
+using namespace std;
+
+User::User(int userId, string name, string phone) : userId(userId), name(name), phone(phone), role("user") {
+
+}
+
+User::User(string name, string phone) : userId(0), name(name), phone(phone), role("admin") {
+
+}
+
+int User::getUserId() {
+    return this->userId;
+}
+
+string User::getName() {
+    return this->name;
+}
+
+string User::getPhone() {
+    return this->phone;
+}
+
+string User::getRole() {
+    return this->role;
+}
+
+void User::printInfo() {
+    // For 디버깅
+    cout << "======== UserInfo ========" << endl;
+    cout << "userId = " << this->userId << endl;
+    cout << "name = " << this->name << endl;
+    cout << "phone = " << this->phone << endl;
+    cout << "role = " << this->role << endl;
+    cout << "==========================" << endl;
+}
+
