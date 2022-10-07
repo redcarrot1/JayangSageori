df4a036a8f5cb15d6dfb9294ea8cf6ed83e037b5
SeoungJun <jameskevinpsj@gmail.com>
2022-10-07 18:29:32 +0900

    refactor: 출력문 제거& 문장 마지막 공백류 처리

--- src/Parsing.cpp
+++ src/Parsing.cpp
@@ -10,17 +10,22 @@ vector<string> Parsing::split(vector<string> line)
 		if (target[end] == '\t' || target[end] == ' ') {
 			if (target[start] == '\t' || target[start] == ' '){
 			//공백이 연속으로 2개가 들어온 경우 하나로 간주
-				start = end + 1;
 			}
-			else {
+			else
+			{
 				result.push_back(target.substr(start, end - start));
-				start = end + 1;
 			}
+			start = end + 1;
 		}
 
 		if (end++ == target.length()-1) {
-			result.push_back(target.substr(start, end - start));
-			cout << end << '\t' << result.size();
+			if (target[start-1] == '\t' || target[start-1] == ' ') {
+				break;
+			}
+			else
+			{
+				result.push_back(target.substr(start, end - start));
+			}
 			break;
 		}
 	}
