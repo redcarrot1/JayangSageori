36ea6ade4e0ac375e8b1f37f9b2ace37d640a83c
MINO <whyknow0604@naver.com>
2022-10-10 22:41:52 +0900

    fix : book.cpp, check.cpp, list.cpp bug fix

--- src/Book.cpp
+++ src/Book.cpp
@@ -8,7 +8,6 @@ Book::Book(string sdate, string sRoomNumber, string sUseStartTime, string sUseEn
 	string startMinute = sUseStartTime.substr(2, 2); stringstream sStartMinute(startMinute); int minuteStart; sStartMinute >> minuteStart; this->startMin = minuteStart;
 	string endHour = sUseEndTime.substr(0, 2); stringstream sEndHour(endHour); int hourEnd; sEndHour >> hourEnd; this->endHour = hourEnd;
 	string endMinute = sUseEndTime.substr(2, 2); stringstream sEndMinute(endMinute);int minuteEnd; sEndMinute >> minuteEnd; this->endMin = minuteEnd;
-	this->sdate = sdate;
 	sIndex = (this->startHour - 9) * 2;
 	eIndex = (this->startMin - 9) * 2;
 	if (this->startMin == 30) {
@@ -17,11 +16,13 @@ Book::Book(string sdate, string sRoomNumber, string sUseStartTime, string sUseEn
 	if (this->endMin == 30) {
 		eIndex += 1;
 	}
-	this->sdate.erase(remove(sdate.begin(), sdate.end(), '-'));
+	sdate.erase(remove(sdate.begin(), sdate.end(), '-'),sdate.end());
+	this->sdate = sdate;
+	cout << this->sdate << endl; //테스트 코드
 	bookFileData = File::getBooking(sdate);
 	userData = File::getUserData(userId);
-	cout << bookFileData.size();
-	cout << userData.size();
+	cout << bookFileData.size() << endl; //테스트 코드
+	cout << userData.size() << endl; //테스트 코드
 }
 
 bool Book::checkReservation(){
--- src/Check.cpp
+++ src/Check.cpp
@@ -3,12 +3,16 @@
 Check::Check(string userid) {
 	this->userid = userid;
 	this->userData = File::getUserData(this->userid);
+	cout << userData.size();
 }
 
 void Check::showReservationList() {
 	int row = userData.size();
+	cout << row << endl;
+	//getUserData에서 데이터를 읽어오지 못해서 발생하는 에러입니다.
 	int col = userData[0].size();
 	// 출력문은 작동 확인되면 수정하겠습니다.
+	cout << col << endl;
 	for (int i = 0; i < row; i++) {
 		for (int j = 0; j < col; j++) {
 			cout << userData[i][j] << " ";
